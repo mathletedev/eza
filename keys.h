1b084486b27d4ec2a3b27e2914832120d36c0f75
Neo Delta <mathletedev@gmail.com>
2022-07-02 23:16:08 -0700

    idk, this might not even be possible

---
+++ keys.h
@@ -0,0 +1,173 @@
+#include <X11/X.h>
+#include <X11/Xlib.h>
+#include <X11/keysym.h>
+#include <iostream>
+
+KeySym charToKeysym(char c) {
+  switch (c) {
+  case ' ':
+    return XK_space;
+  case '1':
+    return XK_1;
+  case '2':
+    return XK_2;
+  case '3':
+    return XK_3;
+  case '4':
+    return XK_4;
+  case '5':
+    return XK_5;
+  case '6':
+    return XK_6;
+  case '7':
+    return XK_7;
+  case '8':
+    return XK_8;
+  case '9':
+    return XK_9;
+  case '0':
+    return XK_0;
+  case 'A':
+    return XK_A;
+  case 'B':
+    return XK_B;
+  case 'C':
+    return XK_C;
+  case 'D':
+    return XK_D;
+  case 'E':
+    return XK_E;
+  case 'F':
+    return XK_F;
+  case 'G':
+    return XK_G;
+  case 'H':
+    return XK_H;
+  case 'I':
+    return XK_I;
+  case 'J':
+    return XK_J;
+  case 'K':
+    return XK_K;
+  case 'L':
+    return XK_L;
+  case 'M':
+    return XK_M;
+  case 'N':
+    return XK_N;
+  case 'O':
+    return XK_O;
+  case 'P':
+    return XK_P;
+  case 'Q':
+    return XK_Q;
+  case 'R':
+    return XK_R;
+  case 'S':
+    return XK_S;
+  case 'T':
+    return XK_T;
+  case 'U':
+    return XK_U;
+  case 'V':
+    return XK_V;
+  case 'W':
+    return XK_W;
+  case 'X':
+    return XK_X;
+  case 'Y':
+    return XK_Y;
+  case 'Z':
+    return XK_Z;
+  case 'a':
+    return XK_a;
+  case 'b':
+    return XK_b;
+  case 'c':
+    return XK_c;
+  case 'd':
+    return XK_d;
+  case 'e':
+    return XK_e;
+  case 'f':
+    return XK_f;
+  case 'g':
+    return XK_g;
+  case 'h':
+    return XK_h;
+  case 'i':
+    return XK_i;
+  case 'j':
+    return XK_j;
+  case 'k':
+    return XK_k;
+  case 'l':
+    return XK_l;
+  case 'm':
+    return XK_m;
+  case 'n':
+    return XK_n;
+  case 'o':
+    return XK_o;
+  case 'p':
+    return XK_p;
+  case 'q':
+    return XK_q;
+  case 'r':
+    return XK_r;
+  case 's':
+    return XK_s;
+  case 't':
+    return XK_t;
+  case 'u':
+    return XK_u;
+  case 'v':
+    return XK_v;
+  case 'w':
+    return XK_w;
+  case 'x':
+    return XK_x;
+  case 'y':
+    return XK_y;
+  case 'z':
+    return XK_z;
+  default:
+    return 0;
+  }
+}
+
+XKeyEvent createKeyEvent(Display *dp, Window &win, Window &root, bool down,
+                         int kc, int mod) {
+  XKeyEvent event;
+
+  event.display = dp;
+  event.window = win;
+  event.root = root;
+  event.subwindow = None;
+  event.time = CurrentTime;
+  event.x = 1;
+  event.y = 1;
+  event.x_root = 1;
+  event.y_root = 1;
+  event.same_screen = 1;
+  event.keycode = kc;
+  event.state = mod;
+  event.type = down ? KeyPress : KeyRelease;
+
+  return event;
+}
+
+void sendChar(char c, Display *dp, Window &win, Window &root) {
+  XKeyEvent evt = createKeyEvent(dp, win, root, true,
+                                 XKeysymToKeycode(dp, charToKeysym(c)), 0);
+  XSendEvent(evt.display, evt.window, 1, KeyPressMask, (XEvent *)&evt);
+  evt = createKeyEvent(dp, win, root, false,
+                       XKeysymToKeycode(dp, charToKeysym(c)), 0);
+  XSendEvent(evt.display, evt.window, 1, KeyPressMask, (XEvent *)&evt);
+  XFlush(dp);
+}
+
+void sendString(std::string s, Display *dp, Window &win, Window &root) {
+  for (char &c : s)
+    sendChar(c, dp, win, root);
+}
--- main.cpp
+++ main.cpp
@@ -1,61 +1,49 @@
-#include <X11/X.h>
+#include "keys.h"
 #include <X11/Xlib.h>
 #include <X11/keysym.h>
 #include <iostream>
-
-XKeyEvent createKeyEvent(Display *display, Window &window, Window &root,
-                         bool down, int keycode, int modifiers) {
-  XKeyEvent event;
-
-  event.display = display;
-  event.window = window;
-  event.root = root;
-  event.subwindow = None;
-  event.time = CurrentTime;
-  event.x = 1;
-  event.y = 1;
-  event.x_root = 1;
-  event.y_root = 1;
-  event.same_screen = True;
-  event.keycode = keycode;
-  event.state = modifiers;
-  event.type = down ? KeyPress : KeyRelease;
-
-  return event;
-}
+#include <map>
 
 int main() {
-  Display *display = XOpenDisplay(NULL);
-  if (!display)
+  Display *dp = XOpenDisplay(NULL);
+  if (!dp)
     return 1;
 
-  Window root = XDefaultRootWindow(display);
-  int revert;
+  std::map<char, std::string> map;
+  map['a'] = "hello";
 
-  Window window;
-  XGetInputFocus(display, &window, &revert);
-  XKeyEvent event = createKeyEvent(display, window, root, true, 26, 0);
-  XSendEvent(event.display, event.window, 1, KeyPressMask, (XEvent *)&event);
-  event = createKeyEvent(display, window, root, false, 26, 0);
-  XSendEvent(event.display, event.window, 1, KeyPressMask, (XEvent *)&event);
-  XFlush(display);
+  Window root = XDefaultRootWindow(dp);
+  int rev;
 
-  int keycode = XKeysymToKeycode(display, XK_a);
-  int rv = XGrabKey(display, keycode, AnyModifier, root, 1, GrabModeAsync,
-                    GrabModeAsync);
-  std::cout << rv << std::endl;
+  Window win;
+  XGetInputFocus(dp, &win, &rev);
+  /* XKeyEvent event = createKeyEvent(display, window, root, true, 26, 0); */
+  /* XSendEvent(event.display, event.window, 1, KeyPressMask, (XEvent *)&event);
+   */
+  /* event = createKeyEvent(display, window, root, false, 26, 0); */
+  /* XSendEvent(event.display, event.window, 1, KeyPressMask, (XEvent *)&event);
+   */
+  /* XFlush(display); */
 
-  XSelectInput(display, window, KeyPressMask | KeyReleaseMask);
+  for (std::map<char, std::string>::iterator iter = map.begin();
+       iter != map.end(); ++iter) {
+    XGrabKey(dp, XKeysymToKeycode(dp, charToKeysym(iter->first)), AnyModifier,
+             root, 1, GrabModeAsync, GrabModeAsync);
+  }
+
+  XSelectInput(dp, win, KeyPressMask | KeyReleaseMask);
   while (1) {
-    while (XPending(display)) {
-      XEvent event;
-      XNextEvent(display, &event);
-      uint32_t keycode = event.xkey.keycode;
-      std::cout << keycode << std::endl;
+    while (XPending(dp)) {
+      XEvent evt;
+      XNextEvent(dp, &evt);
+      std::map<char, std::string>::iterator res =
+          map.find(*XKeysymToString(evt.xkey.keycode));
+      if (res != map.end())
+        sendString(res->second, dp, win, root);
     }
   }
 
-  XCloseDisplay(display);
+  XCloseDisplay(dp);
 
   return 0;
 }
